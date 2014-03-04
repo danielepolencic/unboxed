0e8db52956c0f1e73fcfe0aef6e3b8b4333bd0b8
Daniele Polencic <daniele.polencic@gmail.com>
2014-03-04 11:49:44 +0000

    get the top languages from the api

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,32 @@
+(function () {
 
+  this.Unboxed = this.Unboxed || {};
+
+  var request = this.Unboxed.request({base: 'https://api.github.com'});
+
+  var getTopLanguages = function (username) {
+    return request.get('/users/danielepolencic/repos')
+    .then(function (repos) {
+      var languages_in_repos = repos.map(function (repo) {
+        return request.get('/repos/danielepolencic/' + repo.name + '/languages')
+        .then(function (languages) {
+          return languages;
+        });
+      });
+      return $.when.apply($, languages_in_repos);
+    })
+    .then(function () {
+      var repos = [].slice.call(arguments);
+      return repos.reduce(function (accumulator, repo) {
+
+        Object.keys(repo).forEach(function (language) {
+          if (!(language in accumulator)) accumulator[language] = 0;
+          accumulator[language] += parseInt(repo[language], 10);
+        });
+
+        return accumulator;
+      }, {});
+    });
+  };
+
+}).call(this);
