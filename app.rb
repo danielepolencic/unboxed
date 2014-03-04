0f9e6c2723a68ec3bbef5a09a611bb320be14976
Daniele Polencic <daniele.polencic@gmail.com>
2014-03-04 10:54:20 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,32 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/assetpack'
+
+class App < Sinatra::Base
+
+  set :root, File.dirname(__FILE__)
+  register Sinatra::AssetPack
+
+  assets {
+    serve '/scripts', from: 'assets/scripts'
+    serve '/stylesheets', from: 'assets/stylesheets'
+    serve '/bower_components', from: 'assets/bower_components'
+
+    js :application, '/scripts/application.js', [
+      '/scripts/main.js'
+    ]
+
+    css :application, '/stylesheets/application.css', [
+      '/bower_components/normalize-css/normalize.css',
+      '/stylesheets/style.css',
+    ]
+
+    js_compression  :jsmin    # :jsmin | :yui | :closure | :uglify
+    css_compression :simple   # :simple | :sass | :yui | :sqwish
+  }
+
+  get '/' do
+    erb :index
+  end
+
+end
